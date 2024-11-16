a4fcf14d4d1352ece194e9d5e9c50d392ea017e7
Artem Sharikov <skylinetmk@mail.ru>
2024-11-16 21:52:36 +0300

    Змейка теперь удлиняется, ее значение тела хранится в массиве. Удленяется она при сталкивании с едой. При сталкивании ГОЛОВЫ с одним из элементов тела - игра заканчиваетя

--- snake.py
+++ snake.py
@@ -37,6 +37,11 @@ def display_message(msg,color):
     #dis.blit(message_text, [display_width/2, display_height/2])
     dis.blit(message_text, [5, display_height / 2])
 
+#Функция отображения тела змейки
+def my_snake(snake_block, snake_list):
+   for x in snake_list:
+       pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
 
 def gameLoop(): #Описываем всю игровую логику в одной функции.
     # переменная статуса игры (продолжаеся или закончена)
@@ -44,6 +49,9 @@ def gameLoop(): #Описываем всю игровую логику в одн
 
     # закрыть игру или продолжить
     game_close = False
+
+    snake_List = []  # Создаём список, в котором будем хранить показатель текущей длины змейки.
+    Length_of_snake = 1
 
 
     # стартовое положение змейки - середина экрана
@@ -102,9 +110,26 @@ def gameLoop(): #Описываем всю игровую логику в одн
         dis.fill(white)
         # рисуем змейку
         pygame.draw.rect(dis, blue, [foodx, foody, snake_block, snake_block])
+        # Создаём список, в котором будет храниться показатель длины змейки при движениях.
+        snake_Head = []
+        snake_Head.append(snake_x)  # Добавляем значения в список при изменении по оси х.
+        snake_Head.append(snake_y)  # Добавляем значения в список при изменении по оси y.
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]  # Удаляем первый элемент в списке длины змейки, чтобы она не увеличивалась сама по себе при движениях.
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
         # рисуем змейку
-        pygame.draw.rect(dis, black, [snake_x, snake_y, snake_block, snake_block])
+        my_snake(snake_block, snake_List)
         pygame.display.update() #обновляем дисплей
+
+        if snake_x == foodx and snake_y == foody:  # Указываем, что в случаях, если координаты головы змейки совпадают с координатами еды, еда появляется в новом месте, а длина змейки увеличивается на одну клетку.
+            foodx = round(random.randrange(0, display_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, display_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+
 
         clock.tick(snake_speed) #скорость игры
 
