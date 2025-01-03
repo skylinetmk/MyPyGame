f9f0ebd8af309d7f89d6a9d6dd43f49d5a1c10a4
Artem Sharikov <skylinetmk@mail.ru>
2025-01-03 19:53:07 +0300

    Переместили файлы обучения в одну папку. Далее персонажу сделали управления движением стрелочками вправо-влево. В прыжке он тоже меняет положение (на переменную hero_go_speed). Переписал управление на pygame.key.get_pressed(). Добавил комментариев.

--- my_learning_scripts/rect_class.py
+++ my_learning_scripts/rect_class.py
@@ -27,7 +27,7 @@ clock = pygame.time.Clock()
 
 jump_force = 25     # сила прыжка
 move = jump_force+1
-
+hero_go_speed = 10 # скорость движения персонажа
 
 #создаем землю
 ground_width = SC_Width
@@ -43,16 +43,31 @@ my_hero.fill(BLUE_COLOR) # цвет персонажа
 my_hero_rect = my_hero.get_rect(centerx=SC_Width // 2) # устанавливаем центр персонажа по X в центре экрана
 my_hero_rect.bottom = ground_rect.y #устанавливаем нижнюю часть героя по верху земли
 
+
+
 while True:
+    #обрабатываем выход из игры
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-        elif event.type == pygame.KEYDOWN:
-            # если нажат пробел и герой стоит на уровне земли- то прыжок
-            if event.key == pygame.K_SPACE and ground_rect.y == my_hero_rect.bottom:
-                move = -jump_force # устанавливаем переменную прыжка в начальное состояние, начинаем с минуса
-
-    if move <= jump_force: # пока не достигнем положительного значения move
+    ''''обрабатываем нажатие клавиш стрелок - движение персонажа
+    если уперлись в границы экрана, то дальше не жвигаемся'''
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        my_hero_rect.x -= hero_go_speed
+        #уперлись в левую границу экрана
+        if my_hero_rect.x < 0:
+            my_hero_rect.x = 0
+    elif keys[pygame.K_RIGHT]:
+        my_hero_rect.x += hero_go_speed
+        # уперлись в левую границу экрана
+        if my_hero_rect.x > SC_Width - my_hero_rect.width:
+            my_hero_rect.x = SC_Width - my_hero_rect.width
+    # если нажат пробел и герой стоит на уровне земли- то прыжок
+    if keys[pygame.K_SPACE] and ground_rect.y == my_hero_rect.bottom:
+        move = -jump_force # устанавливаем переменную прыжка в начальное состояние, начинаем с минуса
+    #обрабатываем прыжок
+    if move <= jump_force: # пока не достигнем положительного значения move совершаем прыжок, причем сначала bootom героя увеличивается, затем уменьшается
         if my_hero_rect.bottom + move < ground_rect.y:
             # скорость прыжка регулируется изменением bottom персонажа
             my_hero_rect.bottom += move # сначала скорость вверх большая, поскольку была с минусом
